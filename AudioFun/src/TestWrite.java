e0a2ac7586033ce82a028dfc6a0fb53f50bd89a1
Gumboy82 <33880742+Gumboy82@users.noreply.github.com>
2021-02-03 18:09:35 -0500

    Add files via upload

---
+++ AudioFun/src/TestWrite.java
@@ -0,0 +1,18 @@
+import javax.sound.sampled.spi.AudioFileWriter;
+import javax.sound.sampled.AudioFileFormat;
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+
+
+
+
+public class TestWrite {
+
+	private File path = new File();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
